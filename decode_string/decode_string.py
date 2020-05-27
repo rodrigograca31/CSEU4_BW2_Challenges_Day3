e27c66c7402e1822151d3517abb0bc2062ca54a2
Rodrigo Graça <rodrigograca31@gmail.com>
2020-05-27 19:50:42 +0100

    feat: ✨ WIP decode string

--- decode_string/decode_string.py
+++ decode_string/decode_string.py
@@ -1,3 +1,10 @@
 class Solution:
     def decodeString(self, s: str) -> str:
-        pass
+        print(s.split("\d"))
+
+        return ""
+
+
+print(Solution().decodeString("3[a]2[bc]"))
+print(Solution().decodeString("3[a2[c]]"))
+print(Solution().decodeString("2[abc]3[cd]ef"))
